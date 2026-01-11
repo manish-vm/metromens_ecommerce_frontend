771e007986f18bf35a7e0ddb8674df060ad18b1a
Manish <manishvm310@gmail.com>
2026-01-11 22:55:36 +0530

    Fix formatting of api.js file

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://node.metromenswear.com' + '/api',
+  baseURL: 'https://node.metromenswear.com/api',
   withCredentials: true
 });;
 
 export default api;
+
 
